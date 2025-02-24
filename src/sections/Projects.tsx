6d2f65df95bee0f4ad6dda124ca86c417ec66eb0
RamonSantos9 <ramonfishh@gmail.com>
2025-02-24 14:21:00 -0300

    Update package-lock.json to remove outdated @next/swc-win32-x64-msvc version and add the latest version; add new images for Vercel and Prisma logos

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import Tag from "@/components/Tag";
 import ProjectCard from "@/components/ProjectCard";
 import Image from "next/image";
+import preview3 from "@/assets/images/preview3.png";
 import preview2 from "@/assets/images/preview2.png";
 import preview from "@/assets/images/preview.png";
 import reactIcon from "@/assets/images/react-logo.png";
@@ -8,6 +9,8 @@ import tailwindIcon from "@/assets/images/tailwind-logo.png";
 import typescriptIcon from "@/assets/images/typescript-logo.png";
 import nextjsIcon from "@/assets/images/next-logo2.png";
 import javascriptIcon from "@/assets/images/javascript-logo.png";
+import prismaIcon from "@/assets/images/prisma-logo.png";
+import vercelIcon from "@/assets/images/vercel-logo.png";
 
 export default function Projects() {
   return (
@@ -21,6 +24,52 @@ export default function Projects() {
           <span className="text-lime-400">projetos criativos.</span>
         </h2>
         <div className="mt-12 max-w-7xl mx-auto grid grid-cols-[repeat(auto-fit,minmax(300px,auto))] justify-items-center gap-8">
+          <ProjectCard
+            title="Ramon-Portfólio"
+            description="Desenvolvido com foco em apresentar os projetos, habilidades e experiências Minha Ramon Santos conhecer melhor o trabalho e a trajetória deste desenvolvedor."
+            description2="Ver Projeto"
+            description2Link="https://ramonsantosportfolio.vercel.app"
+            className="md:col-span-2 lg:col-span-1 group"
+            icones={
+              <>
+                <div className="flex items-left gap-2">
+                  <Image src={reactIcon} alt="React" className="w-6 h-6" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={tailwindIcon}
+                    alt="Tailwind CSS"
+                    className="w-6 h-6"
+                  />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={typescriptIcon}
+                    alt="TypeScript"
+                    className="w-6 h-6"
+                  />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={nextjsIcon} alt="nextjs" className="w-6 h-6" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={javascriptIcon}
+                    alt="javascript"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </>
+            }
+          >
+            <div className="aspect-video flex items-center justify-center">
+              <Image
+                src={preview2}
+                alt="Progresso ao Vivo"
+                className="object-cover group-hover:scale-110 rounded-xl transition duration-300"
+              />
+            </div>
+          </ProjectCard>
           <ProjectCard
             title="Rage-Template"
             description="Um template moderno criado para facilitar a construção de interfaces responsivas e escaláveis. Desenvolvido com TypeScript, Next.js e Tailwind CSS, é a base perfeita para projetos de estudo e experimentação."
@@ -68,22 +117,15 @@ export default function Projects() {
             </div>
           </ProjectCard>
           <ProjectCard
-            title="Ramon-Portfólio"
-            description="Desenvolvido com foco em apresentar os projetos, habilidades e experiências Minha Ramon Santos conhecer melhor o trabalho e a trajetória deste desenvolvedor."
+            title="Rage-Donalds"
+            description="desenvolvido para websites mobile, oferecendo uma experiência única e otimizada para dispositivos móveis. Com foco em performance e escalabilidade, este projeto demonstra a união de tecnologias modernas para criar interfaces atraentes e responsivas."
             description2="Ver Projeto"
-            description2Link="https://ramonsantosportfolio.vercel.app"
+            description2Link="https://rage-donalds.vercel.app/rage-donalds"
             className="md:col-span-2 lg:col-span-1 group"
             icones={
               <>
-                <div className="flex items-left gap-2">
-                  <Image src={reactIcon} alt="React" className="w-6 h-6" />
-                </div>
                 <div className="flex items-center gap-2">
-                  <Image
-                    src={tailwindIcon}
-                    alt="Tailwind CSS"
-                    className="w-6 h-6"
-                  />
+                  <Image src={nextjsIcon} alt="nextjs" className="w-6 h-6" />
                 </div>
                 <div className="flex items-center gap-2">
                   <Image
@@ -93,21 +135,25 @@ export default function Projects() {
                   />
                 </div>
                 <div className="flex items-center gap-2">
-                  <Image src={nextjsIcon} alt="nextjs" className="w-6 h-6" />
-                </div>
-                <div className="flex items-center gap-2">
                   <Image
-                    src={javascriptIcon}
-                    alt="javascript"
+                    src={tailwindIcon}
+                    alt="Tailwind CSS"
                     className="w-6 h-6"
                   />
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <Image src={prismaIcon} alt="prisma" className="w-6 h-6" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={vercelIcon} alt="vercel" className="w-6 h-6" />
                 </div>
               </>
             }
           >
             <div className="aspect-video flex items-center justify-center">
               <Image
-                src={preview2}
+                src={preview3}
                 alt="Progresso ao Vivo"
                 className="object-cover group-hover:scale-110 rounded-xl transition duration-300"
               />
